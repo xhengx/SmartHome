7bb73a10ad8b061301f4d3b986c692cc3ae3bb0a
xiaoheng.liu <xiaoheng.liu@gmail.com>
2016-11-16 10:52:22 +0800

    Revert "基本框架设置"
    This reverts commit e6510f68bbb1df39b592cb455ed2be454b09fbb2.

--- SmartHome.ino
+++ SmartHome.ino
@@ -1,19 +1,9 @@
-#include "HomeSensors.h"
-
-
-HomeSensors s;
 void setup() {
+  // put your setup code here, to run once:
 
-  Serial.begin(115200);
- 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-   String req = "{\"request_sensors\" : true, \"control\":{\"set_curtain_on\":true}}";
-   String result = s.recive_request(req);
-   Serial.println("res:" + result);
-  delay(100000);
+
 }
-
-
